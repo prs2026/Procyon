12ce334d82e35559ed387c1de431ee596c43fc37
ebear2018 <ejfsohn@Gmail.com>
2023-06-15 11:37:52 -0700

    software updartes

--- avionics/flight computer FOX/FOX flight computer/src/main.cpp
+++ avionics/flight computer FOX/FOX flight computer/src/main.cpp
@@ -10,19 +10,28 @@ Adafruit_MPU6050 imu;
 Adafruit_BMP280 bmp;
 Servo canards[4];
 
-float kp = 0.2;
-float ki = 0.01;
+
+
+float localpressure = 1013.25;
+
+float kp = 0.5;
+float ki = 0.1;
 float kd = 0.1;
+
+float kproll = 1;
+float kiroll = 0.1;
+float kdroll = 0.1;
 
 float preverror = 0.1;
 float accumulatederror = 0.1;
-unsigned long prevmillis;
-
 unsigned long uptimemillis;
+int rolloffset = 0;
 // x+ x- y+ y-
 int canardpos[4];
 int canardpins[4] = {25, 26, 14, 13};
-int canardsdefualtpos[4] = {104, 43, 67, 67};
+int canardsdefualtpos[4] = {90,  90, 90, 90};
+
+float drift[3] = {0.55,0.52,0.62};
 
 float mpucalibratedgyros[3];
 float mpucalibratedaccell[3];
@@ -99,6 +108,44 @@ struct datatotransmit
 
 };
 
+struct sensordata
+{
+  float accel_x;
+  float accel_y;
+  float accel_z;
+
+  float gyro_x;
+  float gyro_y;
+  float gyro_z;
+
+  float imu_temp;
+  float baro_temp;
+
+  float baro_alt;
+  float baro_pressure;
+
+};
+
+struct orientation
+{
+  float angle_x;
+  float angle_y;
+  float angle_z;
+};
+
+
+struct prevmillllis
+{
+  unsigned long cycle;
+  unsigned long getdata;
+  unsigned long serial;
+  unsigned long pid;
+  unsigned long controlcycle;
+};
+
+prevmillllis prevmilliss;
+sensordata currentdata;
+orientation currentorientation;
 
 void initImu() {
   Serial.println("Initializing IMU...");
@@ -146,7 +193,7 @@ void initImu() {
     break;
   }
 
-  imu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  imu.setFilterBandwidth(MPU6050_BAND_44_HZ);
   Serial.print("Filter bandwidth set to: ");
   switch (imu.getFilterBandwidth()) {
   case MPU6050_BAND_260_HZ:
@@ -195,16 +242,16 @@ void initbaro() {
 
 float PID(float setpoint, float input, float kp, float ki, float kd) {
   
-  float deltatime = (millis() - prevmillis) / 1000;
+  float deltatime = (millis() - prevmilliss.pid) / 1000;
   float error = setpoint - input;
   float deltaerror = error - preverror;
   accumulatederror += error * deltatime;
-  float p = kp * accumulatederror;
-  float i = ki * error;
+  float p = kp * error;
+  float i = ki * accumulatederror;
   float d = kd * deltaerror;
   preverror = error;
-  prevmillis = uptimemillis;
-  return p + i + d;
+  prevmilliss.pid = uptimemillis;
+  return p;
   
 }
 
@@ -214,7 +261,8 @@ void calibratempu() {
   float valuesgyro[3] = {1,1,1};
   float valuesaccel[3] = {1,1,1};
   int iterations = 0;
-  while (iterations <= 20)
+  int targetiterations = 200;
+  while (iterations <= targetiterations)
   {
     if (millis()-prevtime > 1000)
     {
@@ -244,7 +292,7 @@ void calibratempu() {
       
     }
     
-    if (iterations >= 20)
+    if (iterations >= targetiterations)
     {
       break;
     }
@@ -270,7 +318,7 @@ void calibratempu() {
       mpucalibratedgyros[i] = 0;
     }
     else{
-      mpucalibratedgyros[i] = (valuesgyro[i]-1)/20;
+      mpucalibratedgyros[i] = (valuesgyro[i]-1)/targetiterations;
       Serial.print(valuesgyro[i]-1);
       Serial.print(" ");
     }
@@ -280,12 +328,79 @@ void calibratempu() {
       mpucalibratedaccell[i] = 0;
     }
     else{
-      mpucalibratedaccell[i] = (valuesaccel[i]-1)/20;
+      mpucalibratedaccell[i] = (valuesaccel[i]-1)/targetiterations;
       Serial.println(valuesaccel[i]-1);
     }
   }
 
 }
+
+sensordata getsensordata(){
+  sensors_event_t a, g, temp;
+  imu.getEvent(&a, &g, &temp);
+
+  sensordata data;
+  data.accel_x = a.acceleration.x-mpucalibratedaccell[0];
+  data.accel_y = a.acceleration.y-mpucalibratedaccell[1];
+  data.accel_z = a.acceleration.z-mpucalibratedaccell[2];
+
+  data.gyro_x = g.gyro.x-mpucalibratedgyros[0];
+  data.gyro_y = g.gyro.y-mpucalibratedgyros[1];
+  if (g.gyro.z-mpucalibratedaccell[2]<0.01 && g.gyro.z-mpucalibratedgyros[2]>-0.01)
+  {
+    data.gyro_z = 0;
+  }
+  else{
+    data.gyro_z = g.gyro.z-mpucalibratedgyros[2];
+  }
+
+  data.imu_temp = temp.temperature;
+  data.baro_temp = bmp.readTemperature();
+
+  data.baro_alt = bmp.readAltitude(localpressure);
+  data.baro_pressure = bmp.readPressure();
+  
+  return data;
+}
+
+void sendserialdata(sensordata data,orientation orient){
+  Serial.print("");
+  Serial.print(data.accel_x);
+  Serial.print(", ");
+  Serial.print(data.accel_y);
+  Serial.print(", ");
+  Serial.print(data.accel_z);
+
+  Serial.print(",\t");
+  Serial.print(data.gyro_x);
+  Serial.print(", ");
+  Serial.print(data.gyro_y);
+  Serial.print(", ");
+  Serial.print(data.gyro_z);
+
+  Serial.print(",\t");
+  Serial.print(orient.angle_x);
+  Serial.print(", ");
+  Serial.print(orient.angle_y);
+  Serial.print(", ");
+  Serial.print(orient.angle_z);
+
+  Serial.print(",\t");
+  Serial.print(data.imu_temp);
+  Serial.print(", ");
+  Serial.print(data.baro_alt);
+  Serial.println(",");
+}
+
+orientation computeorientation(sensordata data, orientation orient){
+  float timestep = (uptimemillis-prevmilliss.getdata)*0.1;
+  orient.angle_x = orient.angle_x + (data.gyro_x*drift[0]) * timestep;
+  orient.angle_y = orient.angle_y + (data.gyro_y*drift[1]) * timestep;
+  orient.angle_z = orient.angle_z + (data.gyro_z*drift[2]) * timestep;
+  return orient;
+}
+
+
 void setup() {
   uptimemillis = millis();
   Serial.begin(115200);
@@ -308,39 +423,49 @@ void setup() {
   }
    
   calibratempu();
+  currentdata = getsensordata();
   // put your setup code here, to run once:
 }
 
 void loop() {
   uptimemillis = millis();
+  if (uptimemillis - prevmilliss.getdata > 10)
+    {
+      currentdata = getsensordata();
+      currentorientation = computeorientation(currentdata,currentorientation);
+      prevmilliss.getdata = uptimemillis;
+    }
   // put your main code here, to run repeatedly:
-
-  sensors_event_t a, g, temp;
-  imu.getEvent(&a, &g, &temp);
-
+  
   /* Print out the values */
-  Serial.print("X: ");
-  Serial.print(a.acceleration.x-mpucalibratedaccell[0]);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y-mpucalibratedaccell[1]);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z-mpucalibratedaccell[2]);
-  Serial.print("\t");
+  if (uptimemillis - prevmilliss.serial > 100)
+  {
+    sendserialdata(currentdata,currentorientation);
+    prevmilliss.serial = uptimemillis;
+  }
 
-  Serial.print("X: ");
-  Serial.print(g.gyro.x-mpucalibratedgyros[0]);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y-mpucalibratedgyros[1]);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z-mpucalibratedgyros[2]);
-  Serial.print("\t");
+  if (uptimemillis - prevmilliss.controlcycle > 20)
+  {
+    rolloffset = PID(0, currentorientation.angle_z, kp, ki, kd);
+    canardpos[0] = (canardsdefualtpos[0] + PID(0, currentorientation.angle_x, kp, ki, kd));
+    canardpos[1] = (canardsdefualtpos[1] + (PID(0, currentorientation.angle_x, kp, ki, kd))*-1);
+    canardpos[2] = (canardsdefualtpos[2] + PID(0, currentorientation.angle_y, kp, ki, kd));
+    canardpos[3] = (canardsdefualtpos[3] + (PID(0, currentorientation.angle_y, kp, ki, kd))*-1);
+    
+    canardpos[0] += rolloffset;
+    canardpos[1] += rolloffset;
+    canardpos[2] += rolloffset;
+    canardpos[3] += rolloffset;
 
-  Serial.print("temp: ");
-  Serial.print(temp.temperature);
-  Serial.print("\t");
 
-  Serial.print(F("alt = "));
-  Serial.print(bmp.readAltitude(1020.65)); /* Adjusted to local forecast! */
-  Serial.println(" m");
-  delay(100);
+    canards[0].write(canardpos[0]);
+    canards[1].write(canardpos[1]);
+    canards[2].write(canardpos[2]);
+    canards[3].write(canardpos[3]);
+    prevmilliss.controlcycle = uptimemillis;
+  }
+  
+    
+
+  prevmilliss.cycle = uptimemillis;
 }
