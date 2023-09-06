5fff964cbe372cb2d8720c25c8ffe1068cefb5a9
Polaris Bear <39930563+ebear2018@users.noreply.github.com>
2023-09-05 20:50:29 -0700

    Update main.cpp

--- avionics/Lyra_flight_computer/RP2040/picotesting/src/main.cpp
+++ avionics/Lyra_flight_computer/RP2040/picotesting/src/main.cpp
@@ -3,7 +3,9 @@
 
 
 
-#define LED_BUILTINn 25
+#define LEDRED 9
+#define LEDGREEN 10
+#define LEDBLUE 11
 
 Quaternion base(1,0,0,0);
 Quaternion base2(0,1,0,0);
@@ -17,14 +19,17 @@ void printquat(Quaternion q1){
 
 Quaternion rotate(Quaternion torotate, Quaternion axis,float theta){ // rotate torotate quaterion around quaterion axis by theta
     theta = radians(theta)/2;
+    // construct the rotation quaternion from the input axis and theta values
     Quaternion result;
     Quaternion q(1,0,0,0);
     q.w = cos(theta);
     q.x = axis.x*sin(theta);
     q.y = axis.y*sin(theta);
     q.z = axis.z*sin(theta);
+
     Serial1.println("quaternion to rotate by");
     printquat(q);
+    // rotate the original quaternion by the rotatino quaternion
     Quaternion _q = q.conjugate();
     result = (q*torotate)*_q;
     return result;
@@ -32,28 +37,34 @@ Quaternion rotate(Quaternion torotate, Quaternion axis,float theta){ // rotate t
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTINn,OUTPUT);
-  digitalWrite(LED_BUILTINn, HIGH);
+  pinMode(LEDRED,OUTPUT);
+  pinMode(LEDGREEN,OUTPUT);
+  pinMode(LEDBLUE,OUTPUT);
+  digitalWrite(LEDRED, LOW);
+  digitalWrite(LEDGREEN, HIGH);
+  digitalWrite(LEDBLUE, HIGH);
   Serial1.begin(115200);
   Serial1.println("\n\nrestart");
   basetonorm.normalize();
+  /*
   Quaternion addedquat = base+base2;
   Quaternion multedquat = base*basetonorm;
   Quaternion scalarmult = base*4;
   Quaternion rotatedquat = rotate(base,base2,45);
+  */
   //printquat(base2);
   //printquat(basetonorm);
   //printquat(addedquat);
   //printquat(multedquat);
   //printquat(scalarmult);
-  printquat(rotatedquat);
+  //printquat(rotatedquat);
   
 }
 
 void loop() {
-  digitalWrite(LED_BUILTINn,LOW);
+  digitalWrite(LEDRED,LOW);
   delay(1000);
-  digitalWrite(LED_BUILTINn,HIGH);
+  digitalWrite(LEDRED,HIGH);
   delay(1000);
   Serial1.println("loop");
 
