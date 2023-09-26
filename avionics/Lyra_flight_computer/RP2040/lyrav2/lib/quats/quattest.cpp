00e73507b210d2825e98ddc79cba73b335195b08
ebear2018 <ejfsohn@Gmail.com>
2023-09-26 15:08:37 -0700

    more work on quats

--- avionics/Lyra_flight_computer/RP2040/lyrav2/lib/quats/quattest.cpp
+++ avionics/Lyra_flight_computer/RP2040/lyrav2/lib/quats/quattest.cpp
@@ -5,7 +5,7 @@
 
 Quaternion q(1,1,1,1);
 
-Quaternion q1(0.707,0,0,0.707)  ;
+Quaternion q1(1,0,0,0)  ;
 
 Quaternion rotatezaxis(0,0,0,1);
 Quaternion rotatexaxis(0,1,0,0);
@@ -26,5 +26,7 @@ int main(){
     printquat(qinv);
     printquat(q1rot);
     q1rot = rotate(q1rot,rotatexaxis,45);
+    printquat(q1rot);
+    q1rot = rotate(q1rot,rotatexaxis,-45);
     printquat(q1rot);
 }
