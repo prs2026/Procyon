432e2e10d87446802d23bac07bd931fc87e037f2
ebear2018 <ejfsohn@Gmail.com>
2025-05-05 19:50:07 -0700

    work

--- math code/bendingcalcs.py
+++ math code/bendingcalcs.py
@@ -8,7 +8,6 @@ import numpy as np
 rocket_centerofgravity = 0.4 #m
 
 
-
 class Component():
 
     def __init__(self, diemsions, station, component_type):
@@ -16,21 +15,31 @@ class Component():
         self.station = station #m
         self.component_type = component_type #1 = body tube 2 = nose cone 3 = transition 4 = trapazoidal fin set
         if component_type == 1: #body tube
-            length = self.diemsions[0]
-            self.cp = self.station + 0.5*length
+            self.length = self.diemsions[0]
+            self.cp = self.station + 0.5*self.length
         elif component_type == 2: #nosecone
-            length = self.diemsions[0]
-            self.cp = (2/3)*length
+            self.length = self.diemsions[0]
+            self.cp = (2/3)*self.length
             self.CNa = 2
         elif component_type == 3: #transition
-            length = self.diemsions[0]
-            diaemter1 = self.diemsions[1]
-            diaemter2 = self.diemsions[2]
-            self.cp = self.station + (length/3)*((diaemter1+(diaemter2*2))/diaemter1+diaemter2)
+            self.length = self.diemsions[0]
+            self.diameter1 = self.diemsions[1]
+            self.diameter2 = self.diemsions[2]
+            self.cp = self.station + (self.length/3)*((self.diameter1+(self.diameter2*2))/self.diameter1+self.diameter2)
         elif component_type == 4: #trapazoidal fin set
-            rootchord = self.diemsions[0]
-            semispan = self.diemsions[1]
-            tipchord = self.diemsions[2]
-            sweep = self.diemsions[3]
-            midchord = np.sqrt((rootchord^2)+semispan^2)
-            self.cp = self.station + ((midchord*(rootchord + (2*tipchord)))/(3*(rootchord+tipchord)))+((1/6)*(rootchord+tipchord - ((rootchord*tipchord)/(rootchord-tipchord))))
+            self.rootchord = self.diemsions[0]
+            self.semispan = self.diemsions[1]
+            self.tipchord = self.diemsions[2]
+            self.sweep = self.diemsions[3]
+            self.midchord = np.sqrt((self.rootchord^2)+self.semispan^2)
+            self.cp = self.station + ((self.midchord*(self.rootchord + (2*self.tipchord)))/(3*(self.rootchord+self.tipchord)))+((1/6)*(self.rootchord+self.tipchord - ((self.rootchord*self.tipchord)/(self.rootchord-self.tipchord))))
+
+
+
+Nosecone = Component([12,2],0,2)
+Bodytube = Component([30,2],12,1)
+Fins = Component([6,2.5,4,2],34,4)
+
+
+
+
